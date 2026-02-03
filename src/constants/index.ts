5cb1b659b328b01253572eaffaab675624e686f0
Rema S Nampoothiry <ramasnampoothiry@gmail.com>
2026-02-03 14:46:58 +0530

    adding correct backend serivice to index.ts file

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://ai-interview-backend-6672.onrender.com';
 
 export const API_ENDPOINTS = {
   AUTH: {
